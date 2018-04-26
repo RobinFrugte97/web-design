4079a1613749bbba9fa525654da0fd8675a6cf5b
RobinFrugte97 <robin.frugte@hotmail.com>
2018-04-26 11:06:51 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,250 @@
+var openPlayer = document.querySelectorAll('img')
+var section = document.getElementById('iframesection')
+var closePlayer = document.getElementById('close')
+var uitvouwIframe = document.getElementById('uitvouwIframe')
+var videoInfo = document.getElementById('videoInfo')
+var closeButton = document.getElementById('close')
+var iframe = document.getElementById('iframe')
+
+uitvouwIframe.addEventListener('click', function(){
+  if (videoInfo.getAttribute('Style') !== 'transform: translateX(0%);') {
+    videoInfo.setAttribute('Style', 'transform: translateX(0%);')
+    closeButton.setAttribute('Style', 'transform: translateX(-420%);')
+    iframe.setAttribute('Style', 'transform: translateX(0%);')
+    uitvouwIframe.setAttribute('Style', 'transform: rotate(225deg);')
+  } else {
+    videoInfo.setAttribute('Style', 'transform: translateX(84%);')
+    closeButton.setAttribute('Style', 'transform: translateX(-198%);')
+    iframe.setAttribute('Style', 'transform: translateX(16%);')
+    uitvouwIframe.setAttribute('Style', 'transform: rotate(45deg);')
+  }
+})
+
+var iframeLink = []
+
+for(var i=0; openPlayer.length > i; i++) {
+  openPlayer[i].addEventListener('click', function(e){
+    e.preventDefault()
+    var image = ''
+    var video = document.querySelector('#iframesection iframe')
+    video.src = e.target.getAttribute("data-link");
+    section.classList.remove('videoplayer')
+    section.classList.add('openVideo')
+  })
+}
+
+closePlayer.addEventListener('click', function(){
+  section.classList.remove('openVideo')
+  section.classList.add('videoplayer')
+  section.src = '';
+})
+
+var actionContainer = document.getElementById("actionContainer")
+var techContainer = document.getElementById("techContainer")
+var sportContainer = document.getElementById("sportContainer")
+var musicContainer = document.getElementById("musicContainer")
+var gamingContainer = document.getElementById("gamingContainer")
+var actionRadio = document.getElementById("actionRadio")
+var techRadio = document.getElementById("techRadio")
+var sportRadio = document.getElementById("sportRadio")
+var musicRadio = document.getElementById("musicRadio")
+var gamingRadio = document.getElementById("gamingRadio")
+var actionHeader = document.getElementById("actionHeader")
+var techHeader = document.getElementById("techHeader")
+var sportHeader = document.getElementById("sportHeader")
+var musicHeader = document.getElementById("musicHeader")
+var actionLabel = document.getElementById("actionLabel")
+var techLabel = document.getElementById("techLabel")
+var sportLabel = document.getElementById("sportLabel")
+var musicLabel = document.getElementById("musicLabel")
+var gamingLabel = document.getElementById("gamingLabel")
+var selectedFilters = document.getElementById("selectedFilters")
+var filterOverview = document.getElementById("filterOverview")
+var emptyFilter = document.getElementById('geenFilter')
+var filterKlaar = document.getElementById('showVideos')
+var selectedAction = document.createElement('li')
+selectedAction.innerHTML = 'Action'
+selectedAction.setAttribute('class', 'actionSelection')
+var selectedTech = document.createElement('li')
+selectedTech.innerHTML = 'Tech'
+selectedTech.setAttribute('class', 'techSelection')
+var selectedSport = document.createElement('li')
+selectedSport.innerHTML = 'Sport'
+selectedSport.setAttribute('class', 'sportSelection')
+var selectedMusic = document.createElement('li')
+selectedMusic.innerHTML = 'Sport'
+selectedMusic.setAttribute('class', 'musicSelection')
+var selectedGaming = document.createElement('li')
+selectedGaming.innerHTML = 'Sport'
+selectedGaming.setAttribute('class', 'gamingSelection')
+var selectedAction2 = document.createElement('li')
+selectedAction2.innerHTML = 'Action'
+selectedAction2.setAttribute('class', 'actionSelection')
+var selectedTech2 = document.createElement('li')
+selectedTech2.innerHTML = 'Tech'
+selectedTech2.setAttribute('class', 'techSelection')
+var selectedSport2 = document.createElement('li')
+selectedSport2.innerHTML = 'Sport'
+selectedSport2.setAttribute('class', 'sportSelection')
+var selectedMusic2 = document.createElement('li')
+selectedMusic2.innerHTML = 'Sport'
+selectedMusic2.setAttribute('class', 'musicSelection')
+var selectedGaming2 = document.createElement('li')
+selectedGaming2.innerHTML = 'Sport'
+selectedGaming2.setAttribute('class', 'gamingSelection')
+
+actionRadio.addEventListener('click', function(){
+  if(actionContainer.classList.contains('gone')) {
+    actionContainer.classList.remove('gone')
+    actionContainer.classList.add('vidContainer')
+    actionHeader.classList.remove('gone')
+    actionLabel.classList.remove('toggleLabelOff')
+    actionLabel.classList.add('toggleLabelOn')
+    selectedAction.innerHTML = 'Action'
+    selectedAction2.innerHTML = 'Action'
+    selectedFilters.append(selectedAction)
+    filterOverview.append(selectedAction2)
+    emptyFilter.setAttribute('Style', 'display: none;')
+    filterKlaar.setAttribute('Style', 'opacity: 1;')
+  } else {
+    actionContainer.classList.remove('vidContainer')
+    actionContainer.classList.add('gone')
+    actionHeader.classList.add('gone')
+    actionLabel.classList.toggle('toggleLabel')
+    actionLabel.classList.remove('toggleLabelOn')
+    actionLabel.classList.add('toggleLabelOff')
+    selectedAction.innerHTML = ''
+    selectedAction2.innerHTML = ''
+    if(actionContainer.classList.contains('gone') && techContainer.classList.contains('gone') && sportContainer.classList.contains('gone') && musicContainer.classList.contains('gone') && gamingContainer.classList.contains('gone')) {
+      emptyFilter.setAttribute('Style', 'display: block;')
+      filterKlaar.setAttribute('Style', 'opacity: 0;')
+    }
+  }
+})
+techRadio.addEventListener('click', function(){
+  if(techContainer.classList.contains('gone')) {
+    techContainer.classList.remove('gone')
+    techContainer.classList.add('vidContainer')
+    techHeader.classList.remove('gone')
+    techLabel.classList.remove('toggleLabelOff')
+    techLabel.classList.add('toggleLabelOn')
+    selectedTech.innerHTML = 'Tech'
+    selectedTech2.innerHTML = 'Tech'
+    selectedFilters.append(selectedTech)
+    filterOverview.append(selectedTech2)
+    emptyFilter.setAttribute('Style', 'display: none;')
+    filterKlaar.setAttribute('Style', 'opacity: 1;')
+  } else {
+    techContainer.classList.remove('vidContainer')
+    techContainer.classList.add('gone')
+    techHeader.classList.add('gone')
+    techLabel.classList.toggle('toggleLabel')
+    techLabel.classList.remove('toggleLabelOn')
+    techLabel.classList.add('toggleLabelOff')
+    selectedTech.innerHTML = ''
+    selectedTech2.innerHTML = ''
+    if(actionContainer.classList.contains('gone') && techContainer.classList.contains('gone') && sportContainer.classList.contains('gone') && musicContainer.classList.contains('gone') && gamingContainer.classList.contains('gone')) {
+      emptyFilter.setAttribute('Style', 'display: block;')
+      filterKlaar.setAttribute('Style', 'opacity: 0;')
+    }
+  }
+})
+sportRadio.addEventListener('click', function(){
+  if(sportContainer.classList.contains('gone')) {
+    sportContainer.classList.remove('gone')
+    sportContainer.classList.add('vidContainer')
+    sportHeader.classList.remove('gone')
+    sportLabel.classList.remove('toggleLabelOff')
+    sportLabel.classList.add('toggleLabelOn')
+    selectedSport.innerHTML = 'Sport'
+    selectedSport2.innerHTML = 'Sport'
+    selectedFilters.append(selectedSport)
+    filterOverview.append(selectedSport2)
+    emptyFilter.setAttribute('Style', 'display: none;')
+    filterKlaar.setAttribute('Style', 'opacity: 1;')
+  } else {
+    sportContainer.classList.remove('vidContainer')
+    sportContainer.classList.add('gone')
+    sportHeader.classList.add('gone')
+    sportLabel.classList.toggle('toggleLabel')
+    sportLabel.classList.remove('toggleLabelOn')
+    sportLabel.classList.add('toggleLabelOff')
+    selectedSport.innerHTML = ''
+    selectedSport2.innerHTML = ''
+    if(actionContainer.classList.contains('gone') && techContainer.classList.contains('gone') && sportContainer.classList.contains('gone') && musicContainer.classList.contains('gone') && gamingContainer.classList.contains('gone')) {
+      emptyFilter.setAttribute('Style', 'display: block;')
+      filterKlaar.setAttribute('Style', 'opacity: 0;')
+    }
+  }
+})
+musicRadio.addEventListener('click', function(){
+  if(musicContainer.classList.contains('gone')) {
+    musicContainer.classList.remove('gone')
+    musicContainer.classList.add('vidContainer')
+    musicHeader.classList.remove('gone')
+    musicLabel.classList.remove('toggleLabelOff')
+    musicLabel.classList.add('toggleLabelOn')
+    selectedMusic.innerHTML = 'Music'
+    selectedMusic2.innerHTML = 'Music'
+    selectedFilters.append(selectedMusic)
+    filterOverview.append(selectedMusic2)
+    emptyFilter.setAttribute('Style', 'display: none;')
+    filterKlaar.setAttribute('Style', 'opacity: 1;')
+  } else {
+    musicContainer.classList.remove('vidContainer')
+    musicContainer.classList.add('gone')
+    musicHeader.classList.add('gone')
+    musicLabel.classList.toggle('toggleLabel')
+    musicLabel.classList.remove('toggleLabelOn')
+    musicLabel.classList.add('toggleLabelOff')
+    selectedMusic.innerHTML = ''
+    selectedMusic2.innerHTML = ''
+    if(actionContainer.classList.contains('gone') && techContainer.classList.contains('gone') && sportContainer.classList.contains('gone') && musicContainer.classList.contains('gone') && gamingContainer.classList.contains('gone')) {
+      emptyFilter.setAttribute('Style', 'display: block;')
+      filterKlaar.setAttribute('Style', 'opacity: 0;')
+    }
+  }
+})
+gamingRadio.addEventListener('click', function(){
+  if(gamingContainer.classList.contains('gone')) {
+    gamingContainer.classList.remove('gone')
+    gamingContainer.classList.add('vidContainer')
+    gamingHeader.classList.remove('gone')
+    gamingLabel.classList.remove('toggleLabelOff')
+    gamingLabel.classList.add('toggleLabelOn')
+    selectedGaming.innerHTML = 'Gaming'
+    selectedGaming2.innerHTML = 'Gaming'
+    selectedFilters.append(selectedGaming)
+    filterOverview.append(selectedGaming2)
+    emptyFilter.setAttribute('Style', 'display: none;')
+    filterKlaar.setAttribute('Style', 'opacity: 1;')
+  } else {
+    gamingContainer.classList.remove('vidContainer')
+    gamingContainer.classList.add('gone')
+    gamingHeader.classList.add('gone')
+    gamingLabel.classList.toggle('toggleLabel')
+    gamingLabel.classList.remove('toggleLabelOn')
+    gamingLabel.classList.add('toggleLabelOff')
+    selectedGaming.innerHTML = ''
+    selectedGaming2.innerHTML = ''
+    if(actionContainer.classList.contains('gone') && techContainer.classList.contains('gone') && sportContainer.classList.contains('gone') && musicContainer.classList.contains('gone') && gamingContainer.classList.contains('gone')) {
+      emptyFilter.setAttribute('Style', 'display: block;')
+      filterKlaar.setAttribute('Style', 'opacity: 0;')
+    }
+  }
+})
+
+
+var filterNietKlaar = document.getElementById('showFilteren')
+var videoSection = document.getElementById('filteredTopics')
+var filterSection = document.getElementById('beginFilter')
+
+filterKlaar.addEventListener('click', function(){
+  videoSection.setAttribute('Style', 'display: block;')
+  filterSection.setAttribute('Style', 'display: none;')
+})
+
+filterNietKlaar.addEventListener('click', function(){
+  filterSection.setAttribute('Style', 'display: flex;')
+  videoSection.setAttribute('Style', 'display: none;')
+})
---
+++ readme.md
@@ -0,0 +1,96 @@
+# Web Design
+
+# Video website
+
+For this course I have made a video website, on which you can watch YouTube video's. You can filter the content to your liking. The website is built for a couple of specific persons, including [Marie](#marie).
+
+# Design principles
+
+- Clarity (1).
+- One primary action per state (6).
+- Progressive Disclosure (9).
+- Strong visual hierarchy (11).
+
+# Feedback Week 1
+
+## Points of attention
+
+There was too much distraction from the main content of the site. The main content of the site, being the YouTube videos, did not get enough space and attention.
+
+![]()
+
+## Updated version
+
+I remove a large part of the website and a large part of the styling. I did this in order to improve the visual hierarchy from the ground up. I split the remaining parts of the website in different parts so I could make the flow more apparent and give each part of the site a good hierarchy.
+
+![]()
+
+![]()
+
+# Feedback Week 2
+
+## Points of attention
+
+There was too little context when you enter the website. You see some kind of filter but you don't know exactly what it's for. It was not clear what a user can do on the website. When you get passed the filter section, you had no feedback on what you had selected.
+
+## Updated version
+
+I added an introduction to the site, so users can know what to expect from the site.
+I also added a section to the video overview page to let the user know what they filters they selected.
+
+![]()
+
+# Content inventory
+
+- Introduction
+- Content filter page
+- Overview of filtered Youtube video's
+- Video page
+- Information per video
+
+# Users
+
+1. Marie
+- Marie is a web designer. Marie is also deaf.
+Marie is one of two people this website is being build for. Recently we had the opportunity to get some insights from her. There are a couple of things deaf people find important on the web. Sign-language is the first language for most deaf people, including Marie. Sign-language has a different vocabulary to 'regular' language. Here are some of the insights Marie gave:
+
+- Marie prefers short sentences. This makes it easier to understand what the text is about.
+- Marie prefers the content to be 'To the point'. She want to understand what the website is about as quickly as possible. She is not looking for sweet-talk, it has to be direct.
+- Marie prefers emotion through text, rather than through color or any other potentially distracting aspect of design.
+
+2. Job
+- Job is a 21 year old guy that studies Software Development by HvA. He enjoys spending time on the computer. He is a regular visitor to the YouTube website. He enjoys watching videos of certain topics.
+
+# User scenario
+
+- Marie is deaf. She wants to watch an action video about snowboarding. When she is watching the video, she wants to know some context about the video.
+- Mitch wants to quickly select the filters he is interested about that particular day, so he can start watching videos of said topic.
+- Marie want to know how the site works as quickly as possible, so she can be on her way as soon as possible.
+
+
+# Test
+
+I tested the website with Marie in person. I asked her to navigate through the website to find out if the flow was clear enough.
+
+![]()
+
+Marie brought up interesting points:
+1. On the overview page, she didn't think that fact that you can scroll is clear enough.
+2. On the overview page, it was not clear enough that you had to click on the videos to open up the video player.
+3. On the overview page, she thought the section where you can see your filter settings could be more clear. Sometimes it would seem like separate words formed a single one.
+4. On the video player page, she thought it would be easier for her if the information section was opened by default, instead of closed by default. This way she can read about the video without clicking first. If she has enough she can always click to close the information section.
+
+Here is how I can fix these problems:
+1. [x] I can put the topics closer to each other, so more topics are visible in one view. I think this is also a form of progressive disclosure.
+2. [ ] I can add a play button on top of the videos, so it is more clear that they are clickable.
+3. [x] I can add space around each filter item on the overview page, so it is more clear to the user what exactly they have filtered.
+4. [ ] I can have the information tab on the video player page be opened up by default. The user can instead close the tab if they don't want to see the information anymore.
+
+
+# Todo
+
+- [x] Add an introduction to the website to provide more context for the user.
+- [x] Add more ways to filter content.
+- [ ] Re-add the search function.
+- [x] Add more information to the video pages such as subtitles, discription.
+- [x] Re-add styling elements.
